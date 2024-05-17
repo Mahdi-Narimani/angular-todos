8894672dc07cd78c9c1d468dc88ec8987a0cab73
Mahdi_Narimani <mahdi.n.a.m.h.n@gmail.com>
2024-05-17 11:24:06 +0330

    fixed responsive

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
-import { MatIconModule } from "@angular/material/icon";
 import { RouterLink } from "@angular/router";
+import { MatIconModule } from "@angular/material/icon";
+
 @Component({
   selector: "app-footer",
   standalone: true,
